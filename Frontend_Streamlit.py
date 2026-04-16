ce15ea9f9fc70cd0052073905d59598d6096c3b6
Suraj Bhosale <bhosalesuraj2012@gmail.com>
2026-04-16 15:34:53 +0530

    Commit changes - Resume Chat History functionality added

--- Frontend_Streamlit.py
+++ Frontend_Streamlit.py
@@ -1,12 +1,82 @@
 import streamlit as st
 from Backend_LangGrapth import chatbot
 from langchain.messages import HumanMessage
+import uuid  # used for creating unique thread IDs for conversations
 
-CONFIG = {"configurable": {"thread_id": "thread-001"}}
+
+# ----------------------------------utility functions-----------------------------------
+# this function generates unique thread id for every new chat
+def generate_thread_id():
+    thread_id = uuid.uuid4()
+    return thread_id
+
+    # This is invoked when the user clicks on the "New Chat" button in the sidebar.
+    # It clears the message history and generates a new thread ID for the new conversation.
+
+
+def reset_chat():
+    thread_id = generate_thread_id()
+    st.session_state["thread_id"] = thread_id
+    add_threads(thread_id)
+    st.session_state["message_hitory"] = []
+
+
+def add_threads(thread_id):
+    if thread_id not in st.session_state["chat_threads"]:
+        st.session_state["chat_threads"].append(thread_id)
+
+    # below function accept a thread_id and return messages(HumanMessage and AIMessages)
+    # that stored in thread with that thread_id.
+    # This is used to display the chat history when user clicks on a thread in the sidebar.
+
+
+def load_conversation(thread_id):
+    return chatbot.get_state(config={"configurable": {"thread_id": thread_id}}).values[
+        "messages"
+    ]
+
+
+# -----------------------------------Session State Initialization-----------------------
 
 # Initialize session state for message history and hero section visibility
 if "message_hitory" not in st.session_state:
     st.session_state["message_hitory"] = []
+
+if "thread_id" not in st.session_state:
+    st.session_state["thread_id"] = generate_thread_id()
+
+if "chat_threads" not in st.session_state:
+    st.session_state["chat_threads"] = []
+
+add_threads(st.session_state["thread_id"])
+
+# -----------------------------------Sidebar UI-----------------------------------------
+st.sidebar.title("LangGraph Chatbot")
+if st.sidebar.button("New Chat"):
+    reset_chat()
+st.sidebar.header("My Conversations")
+
+
+for thread in st.session_state["chat_threads"]:
+    if st.sidebar.button(str(thread)):
+        st.session_state["thread_id"] = thread
+        messages = load_conversation(thread)
+
+        # here we are format the message into a list of dict with role and content,
+        # because the load_conversation return a list of HumanMessage and AIMessages objects,
+        # but we want to store the message history in session state as a list of dict with role and content,
+        # so we need to convert the HumanMessage and AIMessages objects into dict with role and content.
+        temp_messages = []
+        for msg in messages:
+            if isinstance(msg, HumanMessage):
+                role = "user"
+            else:
+                role = "assistant"
+            temp_messages.append({"role": role, "content": msg.content})
+        st.session_state["message_hitory"] = temp_messages
+
+
+# -------------------------------------Main UI--------------------------------------
 if "show_hero" not in st.session_state:
     st.session_state["show_hero"] = True
 
@@ -49,6 +119,7 @@ if user_input:
     # st.session_state["message_hitory"].append(
     #     {"role": "assistant", "content": ai_message}
     # )
+    CONFIG = {"configurable": {"thread_id": st.session_state["thread_id"]}}
     with st.chat_message("assistant"):
         ai_message = st.write_stream(
             message_chunk.content
